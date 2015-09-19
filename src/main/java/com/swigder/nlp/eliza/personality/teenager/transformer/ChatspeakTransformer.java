180545e2d382870a40501f5a4e8b733222e35965
suri <swigder@gmail.com>
2015-09-19 15:28:45 -0400

    adding chatspeak transformer as part of teenager configuration

---
+++ src/main/java/com/swigder/nlp/eliza/personality/teenager/transformer/ChatspeakTransformer.java
@@ -0,0 +1,23 @@
+package com.swigder.nlp.eliza.personality.teenager.transformer;
+
+import com.swigder.nlp.eliza.transformer.OutputTransformer;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Transforms words into their chatspeak equivalent
+ * Ex: 'you' into 'u' (includes derived words like 'your', 'yourself').
+ *     'are' into 'r'
+ */
+@Component
+@Order(value = 1)
+public class ChatspeakTransformer implements OutputTransformer {
+
+    @Override
+    public String transform(String input) {
+        input = input.replaceAll("you", "u");
+        input = input.replaceAll("\\sare\\s", " r ");
+        input = input.replaceAll("[Ww]hy\\s", "y ");
+        return input;
+    }
+}
